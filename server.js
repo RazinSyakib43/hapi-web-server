8b12c4b7defcae66a4ab349892116d266e60a222
Razin Syakib <muhammadrazin14@gmail.com>
2023-03-17 22:43:26 +0700

    Routing awalan

--- server.js
+++ server.js
@@ -7,6 +7,23 @@ const init = async () => {
         port: 5000,
         host: 'localhost',
     });
+
+    server.route([
+        {
+            method: 'GET',
+            path: '/',
+            handler: (request, h) => {
+                return 'Homepage';
+            },
+        },
+        {
+            method: 'GET',
+            path: '/about',
+            handler: (request, h) => {
+                return 'About Page';
+            },
+        },
+    ]);
  
     await server.start();
     console.log(`Server berjalan pada ${server.info.uri}`);
